28fb99e9c018465555a0c9c0dc92d81e0094e962
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-24 21:34:38 +0800

    添加 MapStruct 转换器

--- xiwenlejian-user/src/main/java/com/smallc/xiwenlejian/user/service/impl/UserServiceImpl.java
+++ xiwenlejian-user/src/main/java/com/smallc/xiwenlejian/user/service/impl/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package com.smallc.xiwenlejian.user.service.impl;
 
 import com.smallc.xiwenlejian.common.user.bo.UserBO;
+import com.smallc.xiwenlejian.user.mapper.UserMapper;
 import com.smallc.xiwenlejian.user.vo.UserVO;
 import com.smallc.xiwenlejian.user.dto.UserDTO;
-import com.smallc.xiwenlejian.user.mapper.UserMapper;
+import com.smallc.xiwenlejian.user.dao.UserDao;
 import com.smallc.xiwenlejian.user.model.User;
 import com.smallc.xiwenlejian.user.service.UserService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,32 +21,26 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     @Autowired
-    private UserMapper userMapper;
+    private UserDao userDao;
 
     @Override
-    public UserVO login(UserDTO user) {
-        User userDO = userMapper.getByUsername(user.getUsername());
-        if (!userDO.getPassword().equals(user.getPassword())) {
+    public UserVO login(UserDTO userDTO) {
+        User user = userDao.getByUsername(userDTO.getUsername());
+        if (!user.getPassword().equals(userDTO.getPassword())) {
             return null;
         }
-        UserVO userVO = new UserVO();
-        BeanUtils.copyProperties(userDO, userVO);
-        return userVO;
+        return UserMapper.INSTANCE.convertToVO(user);
     }
 
     @Override
     public UserVO getInfo(Long userId) {
-        User userDO = userMapper.getByUserId(userId);
-        UserVO userVO = new UserVO();
-        BeanUtils.copyProperties(userDO, userVO);
-        return userVO;
+        User user = userDao.getByUserId(userId);
+        return UserMapper.INSTANCE.convertToVO(user);
     }
 
     public UserBO getByUserId(Long userId) {
-        User userDO = userMapper.getByUserId(userId);
-        UserBO userBO = new UserBO();
-        BeanUtils.copyProperties(userDO, userBO);
-        return userBO;
+        User user = userDao.getByUserId(userId);
+        return UserMapper.INSTANCE.convertToBO(user);
     }
 
 }
