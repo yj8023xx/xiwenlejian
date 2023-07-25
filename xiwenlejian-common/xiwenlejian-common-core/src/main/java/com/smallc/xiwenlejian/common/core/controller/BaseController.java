71d2ac0ab34e7b50a00a2a69c74f652de3ad6140
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-25 19:35:41 +0800

    修改为 RESTful 风格

--- xiwenlejian-common/xiwenlejian-common-core/src/main/java/com/smallc/xiwenlejian/common/core/controller/BaseController.java
+++ xiwenlejian-common/xiwenlejian-common-core/src/main/java/com/smallc/xiwenlejian/common/core/controller/BaseController.java
@@ -1,8 +1,7 @@
 package com.smallc.xiwenlejian.common.core.controller;
 
 
-import com.smallc.xiwenlejian.common.core.constant.Constant;
-import com.smallc.xiwenlejian.common.core.vo.ResultVO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,12 +19,8 @@ public class BaseController {
      *
      * @return
      */
-    public ResultVO onSuccess(Object data) {
-        return new ResultVO(Constant.RESPONSE_CODE_SUCCESS, Constant.RESPONSE_MSG_SUCCESS, data);
-    }
-
-    public ResultVO onSuccess() {
-        return new ResultVO(Constant.RESPONSE_CODE_SUCCESS, Constant.RESPONSE_MSG_SUCCESS);
+    public <T> ResponseEntity<T> onSuccess(T data) {
+        return ResponseEntity.ok(data);
     }
 
     /**
@@ -33,20 +28,8 @@ public class BaseController {
      *
      * @return
      */
-    public ResultVO onFail(Integer code, String msg) {
-        return new ResultVO(code, msg);
-    }
-
-    public ResultVO onFail(Integer code) {
-        return new ResultVO(code, Constant.RESPONSE_MSG_FAIL);
-    }
-
-    public ResultVO onFail(String msg) {
-        return new ResultVO(Constant.RESPONSE_CODE_FAIL, msg);
-    }
-
-    public ResultVO onFail() {
-        return new ResultVO(Constant.RESPONSE_CODE_FAIL, Constant.RESPONSE_MSG_FAIL);
+    public ResponseEntity onFail(int status, String msg) {
+        return ResponseEntity.status(status).body(msg);
     }
 
 }
