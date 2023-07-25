3b8e68db241acaeb2ebe9d39ce7aa06f53bd4207
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-25 19:33:44 +0800

    修改为了 RESTful 风格

--- xiwenlejian-user/src/main/java/com/smallc/xiwenlejian/user/controller/UserController.java
+++ xiwenlejian-user/src/main/java/com/smallc/xiwenlejian/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.smallc.xiwenlejian.user.controller;
 
 import com.smallc.xiwenlejian.common.core.controller.BaseController;
-import com.smallc.xiwenlejian.common.core.vo.ResultVO;
 import com.smallc.xiwenlejian.user.vo.UserVO;
 import com.smallc.xiwenlejian.user.dto.UserDTO;
 import com.smallc.xiwenlejian.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,19 +32,24 @@ public class UserController extends BaseController {
      * @return
      */
     @PostMapping("/login")
-    public ResultVO login(@RequestBody UserDTO user) {
+    public ResponseEntity<UserVO> login(@RequestBody UserDTO user) {
         UserVO userVO = userService.login(user);
         if (userVO == null) {
-            return onFail("用户名或密码错误");
+            return onFail(HttpStatus.UNAUTHORIZED.value(),"用户名或密码错误");
         }
         return onSuccess(userVO);
     }
 
-    @GetMapping
-    public ResultVO getInfo(@RequestParam("userId") Long userId) {
+    /**
+     *
+     * @param userId 用户的id
+     * @return
+     */
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserVO> getInfo(@PathVariable Long userId) {
         UserVO userVO = userService.getInfo(userId);
         if (userVO == null) {
-            return onFail("用户不存在");
+            return onFail(HttpStatus.NOT_FOUND.value(), "用户不存在");
         }
         return onSuccess(userVO);
     }
