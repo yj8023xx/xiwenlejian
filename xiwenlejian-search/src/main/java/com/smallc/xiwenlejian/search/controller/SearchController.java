7160110109376fefec53ba09321327c7ecf96ac4
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-25 19:32:08 +0800

    修改为 RESTful 风格

--- xiwenlejian-search/src/main/java/com/smallc/xiwenlejian/search/controller/SearchController.java
+++ xiwenlejian-search/src/main/java/com/smallc/xiwenlejian/search/controller/SearchController.java
@@ -1,10 +1,12 @@
 package com.smallc.xiwenlejian.search.controller;
 
+import com.smallc.xiwenlejian.common.core.controller.BaseController;
 import com.smallc.xiwenlejian.common.core.vo.PageResultVO;
 import com.smallc.xiwenlejian.search.dto.SearchDTO;
 import com.smallc.xiwenlejian.search.service.SearchService;
 import com.smallc.xiwenlejian.search.vo.BookVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,14 +16,18 @@ import org.springframework.web.bind.annotation.*;
  * @since com.smallc.xiwenlejian.search.controller
  */
 @RestController
-public class SearchController {
+public class SearchController extends BaseController {
 
     @Autowired
     private SearchService searchService;
 
-    @GetMapping("/search")
-    PageResultVO<BookVO> searchBooks(@ModelAttribute SearchDTO searchDTO) {
-        return searchService.searchBooks(searchDTO);
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<PageResultVO<BookVO>> searchBooks(@PathVariable String keyword,
+                                                            @RequestParam(required = false, defaultValue = "1") Integer pageNum,
+                                                            @RequestParam(required = false, defaultValue = "12")Integer pageSize) {
+        SearchDTO searchDTO = new SearchDTO(keyword, pageNum, pageSize);
+        PageResultVO<BookVO> result = searchService.searchBooks(searchDTO);
+        return onSuccess(result);
     }
 
 }
