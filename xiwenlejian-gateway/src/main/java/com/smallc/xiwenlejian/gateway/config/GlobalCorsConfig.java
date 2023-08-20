689b7a84d17e229e88ae0d7c63aac0b240ae0d31
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-08-20 18:41:26 +0800

    Add cross domain support

---
+++ xiwenlejian-gateway/src/main/java/com/smallc/xiwenlejian/gateway/config/GlobalCorsConfig.java
@@ -0,0 +1,32 @@
+package com.smallc.xiwenlejian.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author yj8023xx
+ * @version 1.0
+ * @date 2023/8/20
+ * @since com.smallc.xiwenlejian.gateway.config
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+}
