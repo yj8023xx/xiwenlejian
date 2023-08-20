2cbc841f36950a9c7fe2f8913d750fad0502f58f
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-08-20 18:47:24 +0800

    Update BookFeignClient.java

--- xiwenlejian-api/xiwenlejian-api-book/src/main/java/com/smallc/xiwenlejian/api/book/feign/BookFeignClient.java
+++ xiwenlejian-api/xiwenlejian-api-book/src/main/java/com/smallc/xiwenlejian/api/book/feign/BookFeignClient.java
@@ -3,6 +3,8 @@ package com.smallc.xiwenlejian.api.book.feign;
 import com.smallc.xiwenlejian.common.book.bo.BookBO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,19 +18,22 @@ import java.util.List;
 @FeignClient("xiwenlejian-book")
 public interface BookFeignClient {
 
-    @GetMapping("/getByBookId")
-    BookBO getByBookId(@RequestParam("bookId") Long bookId);
+    @GetMapping("/feign/book/{bookId}")
+    BookBO getByBookId(@PathVariable("bookId") Long bookId);
 
-    @GetMapping("/listByOrder")
+    @GetMapping("/feign/book/list")
+    List<BookBO> listByBookIds(@RequestBody List<Long> ids);
+
+    @GetMapping("/feign/book/sorted")
     List<BookBO> listByOrder(@RequestParam("orderBy") String orderBy, @RequestParam("size") Integer size);
 
-    @GetMapping("/listByAuthor")
-    List<BookBO> listByAuthor(@RequestParam("author") String author, @RequestParam("size") Integer size);
+    @GetMapping("/feign/book/author/{author}")
+    List<BookBO> listByAuthor(@PathVariable("author") String author, @RequestParam("size") Integer size);
 
-    @GetMapping("/listByPublisher")
+    @GetMapping("/feign/book/publisher/{publisher}")
     List<BookBO> listByPublisher(@RequestParam("publisher") String publisher, @RequestParam("size") Integer size);
 
-    @GetMapping("/listHistoryBooks")
-    List<BookBO> listHistoryBooks(@RequestParam("userId") Long userId, @RequestParam("size") Integer size);
+    @GetMapping("/feign/book/highrated/{userId}")
+    List<BookBO> listRecentHighRatedBooks(@PathVariable("userId") Long userId, @RequestParam("size") Integer size);
 
 }
